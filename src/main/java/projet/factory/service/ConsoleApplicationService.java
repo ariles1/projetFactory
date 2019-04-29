e8e6069d4346bcb56e3a80c5d98862134982b485
Pierre Boucher <pierreboucher@MacBook-Pro-de-Pierre.local>
2019-04-29 14:32:25 +0200

    ajout de code dans ConsoleApplicationService pour mettre votre premier user password encodé

--- src/main/java/projet/factory/service/ConsoleApplicationService.java
+++ src/main/java/projet/factory/service/ConsoleApplicationService.java
@@ -1,15 +1,29 @@
 package projet.factory.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import projet.factory.entity.User;
+import projet.factory.repository.UserRepository;
 
 @Service
 public class ConsoleApplicationService implements CommandLineRunner{
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		
+	/*
+		for (User user : userRepository.findAll()) {
+			user.setPassword(passwordEncoder.encode(user.getPassword()));
+			userRepository.save(user);
+		}
+	*/
 	}
-
 }
